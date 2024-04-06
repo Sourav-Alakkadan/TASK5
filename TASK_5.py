4bf27367835dc5b92874e757f9202b3b07e9b0d6
souravmathil2281@gmail.com <souravmathil2281@gmail.com>
2024-04-06 15:15:52 +0530

    initial

---
+++ TASK_5.py
@@ -0,0 +1,28 @@
+# #1.Write a NumPy program to test whether none of the elements of a given array are zero
+# import numpy as np
+# x=np.array([1,5,4,8,0,2])
+# print(np.all(x))
+
+# #2.Write a NumPy program to create an array of integers from 30 to 70.
+# import numpy as np
+# x=np.arange(30,71)
+# print(x)
+
+# #3.Write a NumPy program to create an array of all even integers from 30 to 70.
+# import numpy as np
+# x=np.arange(30,71,2)
+# print(x)
+
+# #4. Get the common items between a and b
+# #Input:
+# import numpy as np
+# a = np.array([1,2,3,2,3,4,3,4,5,6])
+# b = np.array([7,2,10,2,7,4,9,4,9,8])
+# print(np.intersect1d(a,b))
+
+# #5.Reverse the columns of a 2D array arr.
+# # Input
+# import numpy as np
+# arr = np.arange(9).reshape(3,3)
+# print(arr)
+# print(arr[:,::-1])
